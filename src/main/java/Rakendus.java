d047c67849e55463b40d7b47d4c677756b3c90d5
kuznetsovatatjana <kuznetsovatatjana@gmail.com>
2017-03-10 15:06:54 +0200

    Update Rakendus.java

--- src/main/java/Rakendus.java
+++ src/main/java/Rakendus.java
@@ -8,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Rakendus {
 	@RequestMapping("/geomeetriline_keskmine")
-	String gk(String arv1, String arv2){
+	String gk(String arv1, String arv2, String arv3){
 		if(arv1==null){return "andmed puuduvad";}
 		int a1=Integer.parseInt(arv1);
 		int a2=Integer.parseInt(arv2);
-		return arv1+" ja "+arv2 + " geomeetriline keskmine on "+ Math.sqrt(a1*a2);
+		int a2=Integer.parseInt(arv3);
+		return arv1+" ja "+arv2 + " ja "+arv3+" geomeetriline keskmine on "+ Math.sqrt(a1*a2*a3);
 		
 	}
 	
