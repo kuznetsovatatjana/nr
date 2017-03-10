271042cd8c59cd7137ff5926b76c38b6e98de188
kuznetsovatatjana <kuznetsovatatjana@gmail.com>
2017-03-10 13:39:03 +0200

    Create Rakendus.java

---
+++ Rakendus.java
@@ -0,0 +1,32 @@
+package tatjkuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Rakendus {
+	@RequestMapping("/algus")
+    String tervitusfunktsioon() {
+        return "Ahoi!";
+    }
+	@RequestMapping("/tervita")
+	String tervitusfunktsioon3(String eesnimi){
+		return "Tere "+eesnimi+"!";
+	}
+
+	@RequestMapping("/algus2")
+	String tervitusfunktsioon2(){
+		return "Hello World!";
+	}
+ 
+    public static void main(String[] args) {
+		System.getProperties().put("server.port", 5555);
+        SpringApplication.run(Rakendus.class, args);
+    }
+}
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot3-1.0-SNAPSHOT.jar
